9c49c858210bee8bdd80b0efdfc75a04b292ca7c
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-11 18:45:44 -0500

    add initial path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from '@tailwindcss/vite';
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-    react(), 
+    tsconfigPaths(),
     tailwindcss(),
+    react(), 
   ],
 });

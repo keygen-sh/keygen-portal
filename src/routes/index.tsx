729eeca2c88dc9006728c9ab84927e405f83d173
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-15 17:42:11 -0500

    add redirect route to router on root init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
+import * as Page from "@pages/index";
 
 export const Route = createFileRoute("/")({
-  component: Index,
+  component: () => <Page.Redirect />,
 });
-
-function Index() {
-  return <div>You're on the home page.</div>;
-}

64e4e6e08d93463fe6b20aabae06ac164e15897f
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-15 17:40:32 -0500

    add initial auth routes to router

---
+++ src/routes/auth.login.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router";
+import * as Page from "@pages/index";
+
+export const Route = createFileRoute("/auth/login")({
+  component: () => <Page.Auth.Login />,
+});
--- src/routes/login.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/login")({
-  component: Login,
-});
-
-function Login() {
-  return <div>You're on the login page.</div>;
-}

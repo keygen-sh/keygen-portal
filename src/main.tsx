8e763117b107ec8c19be794c59c2e31c49de532b
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-15 17:26:51 -0500

    add fallback error route to router

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+import * as Page from "@pages/index";
+
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: () => <Page.Error />,
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -15,6 +20,7 @@ const rootElement = document.getElementById("root")!;
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
+
   root.render(
     <StrictMode>
       <RouterProvider router={router} />

05453b4cbdb2876d868fd202e94d12aff78b3ea7
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-20 07:34:02 -0500

    add login password form

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "@tanstack/react-router";
+import { useNavigate, Link } from "@tanstack/react-router";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -20,16 +20,35 @@ const emailSchema = z.object({
   username: z.string().email("Please enter a valid email."),
 });
 
+const passwordSchema = z.object({
+  password: z.string().min(8, "Password must be at least 8 characters."),
+  remember: z.boolean().optional(),
+});
+
 export default function Login() {
   const [step, setStep] = useState(1);
+  const navigate = useNavigate();
 
   const emailForm = useForm<z.infer<typeof emailSchema>>({
     resolver: zodResolver(emailSchema),
     defaultValues: { username: "" },
   });
 
+  const passwordForm = useForm<z.infer<typeof passwordSchema>>({
+    resolver: zodResolver(passwordSchema),
+    defaultValues: {
+      password: "",
+      remember: false,
+    },
+  });
+
   function onSubmitEmail() {
     setStep(2);
+  }
+
+  function onSubmitPassword() {
+    // TODO: Handle auth
+    void navigate({ to: "/app/home" });
   }
 
   const renderEmailForm = () => (
@@ -90,9 +109,75 @@ export default function Login() {
     </section>
   );
 
+  const renderPasswordForm = () => (
+    <section className="relative flex w-80 flex-col justify-center">
+      <h1 className="mb-8 bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent">
+        Enter your password
+      </h1>
+
+      <Form {...passwordForm}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            void passwordForm.handleSubmit(onSubmitPassword)(e);
+          }}
+          className="my-3 w-full space-y-7"
+        >
+          <FormField
+            control={passwordForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-content-muted">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    autoComplete="current-password"
+                    placeholder="Enter password..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+
+                <Link
+                  to="/auth/recovery"
+                  className="underline-slide w-fit pb-1 text-sm font-semibold text-secondary"
+                >
+                  Forgot password?
+                </Link>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={passwordForm.control}
+            name="remember"
+            render={({ field }) => (
+              <FormItem className="flex items-center">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel className="text-content-muted">
+                  Remember me on this device
+                </FormLabel>
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" size="lg" className="w-full">
+            Sign in
+          </Button>
+        </form>
+      </Form>
+    </section>
+  );
+
   return (
     <div className="flex w-full flex-col items-center justify-center">
-      {step === 1 ? renderEmailForm() : null}
+      {step === 1 ? renderEmailForm() : renderPasswordForm()}
     </div>
   );
 }

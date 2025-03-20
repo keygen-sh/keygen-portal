afba94f67a749d4c1c77a622532374b986576e1a
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-20 07:20:37 -0500

    add link button styles and size

--- src/assets/components/ui/button.tsx
+++ src/assets/components/ui/button.tsx
@@ -19,13 +19,14 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+        link: "cursor-pointer text-secondary underline-slide",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
         icon: "size-9",
+        link: "h-6",
       },
     },
     defaultVariants: {

615a1bc1003bf458ea058f19b61ae75aecb4807f
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-20 07:23:27 -0500

    add auth layout styles

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from "@tanstack/react-router";
+import Logo from "@assets/logos/logo.svg";
 
 export default function AuthLayout() {
   return (
-    <main>
-      <div>
-        <h1>Auth Layout</h1>
+    <main className="flex min-h-screen w-full">
+      <div className="relative flex w-1/2 items-center justify-center bg-background">
+        <div className="absolute top-10 left-1/2 flex w-full -translate-x-1/2 items-center justify-center">
+          <img src={Logo} alt="Keygen Logo" className="h-5" />
+        </div>
+        <Outlet />
       </div>
-      <Outlet />
+      <div className="w-1/2 bg-brand-background-2">{/* TODO: Hero */}</div>
     </main>
   );
 }

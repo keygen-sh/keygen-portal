8a281fab5e682f474788de2224a27536a67ea843
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-22 04:26:35 -0500

    update focus styles

--- src/assets/components/ui/button.tsx
+++ src/assets/components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@assets/lib/utils"
 
 const buttonVariants = cva(
-  "select-none inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "select-none inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default:
-          "cursor-pointer bg-primary text-primary-foreground shadow-xs hover:brightness-110",
+          "cursor-pointer bg-primary text-primary-foreground shadow-xs hover:brightness-90 focus-visible:brightness-90",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "cursor-pointer text-secondary underline-slide",
+        link: "cursor-pointer text-secondary underline-slide focus-visible:underline",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",

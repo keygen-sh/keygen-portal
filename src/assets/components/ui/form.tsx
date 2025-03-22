ca52ea50d799337c2222b5b7602cffb2585875f7
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-22 04:25:44 -0500

    remove destructive color on form label

--- src/assets/components/ui/form.tsx
+++ src/assets/components/ui/form.tsx
@@ -91,13 +91,12 @@ function FormLabel({
   className,
   ...props
 }: React.ComponentProps<typeof LabelPrimitive.Root>) {
-  const { error, formItemId } = useFormField()
+  const { formItemId } = useFormField()
 
   return (
     <Label
       data-slot="form-label"
-      data-error={!!error}
-      className={cn("data-[error=true]:text-destructive", className)}
+      className={className}
       htmlFor={formItemId}
       {...props}
     />

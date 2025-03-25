038148fbfe7fdfc47113fc87ceca136b4b4f8160
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-24 19:19:29 -0500

    add responsive styles to auth ui components

--- src/pages/auth/Password.tsx
+++ src/pages/auth/Password.tsx
@@ -37,16 +37,16 @@ export default function Password() {
   }
 
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center">
+    <section className="relative flex h-full w-full flex-col items-center justify-evenly md:justify-center">
       <Button
         variant="link"
         size="link"
-        className="absolute top-10 left-20 w-fit text-content-subdued"
+        className="flex w-80 justify-start text-content-subdued md:absolute md:top-24 md:left-24 md:w-auto"
         asChild
       >
         <Link to="/auth/login">
           <svg
-            className="mt-0.5 h-6 w-6 text-content-subdued dark:text-white"
+            className="mt-0.5 h-6 w-6 text-content-subdued"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -63,72 +63,70 @@ export default function Password() {
           Go back
         </Link>
       </Button>
-      <section className="flex w-80 flex-col justify-center">
-        <h1 className="mb-8 bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent select-none">
-          Enter your password
-        </h1>
 
-        <Form {...passwordForm}>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault()
-              void passwordForm.handleSubmit(onSubmitPassword)(e)
-            }}
-            className="relative my-3 w-full space-y-7"
-          >
-            <FormField
-              control={passwordForm.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="text-content-muted">Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      variant="default"
-                      type="password"
-                      toggle={true}
-                      autoComplete="current-password"
-                      autoFocus
-                      placeholder="Enter your password..."
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
+      <Form {...passwordForm}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            void passwordForm.handleSubmit(onSubmitPassword)(e)
+          }}
+          className="relative my-3 h-1/2 w-80 space-y-7 md:h-auto"
+        >
+          <h1 className="mb-8 bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent select-none">
+            Enter your password
+          </h1>
+          <FormField
+            control={passwordForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-content-muted">Password</FormLabel>
+                <FormControl>
+                  <Input
+                    variant="default"
+                    type="password"
+                    toggle={true}
+                    autoComplete="current-password"
+                    autoFocus
+                    placeholder="Enter your password..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
 
-                  <Link
-                    to="/auth/recovery"
-                    className="underline-slide w-fit pb-0.75 text-sm font-semibold text-secondary select-none"
-                  >
-                    Forgot password?
-                  </Link>
-                </FormItem>
-              )}
-            />
+                <Link
+                  to="/auth/recovery"
+                  className="underline-slide w-fit pb-0.75 text-sm font-semibold text-secondary select-none"
+                >
+                  Forgot password?
+                </Link>
+              </FormItem>
+            )}
+          />
 
-            <FormField
-              control={passwordForm.control}
-              name="remember"
-              render={({ field }) => (
-                <FormItem className="flex items-center">
-                  <FormControl>
-                    <Checkbox
-                      checked={field.value}
-                      onCheckedChange={field.onChange}
-                    />
-                  </FormControl>
-                  <FormLabel className="text-content-muted">
-                    Remember me on this device
-                  </FormLabel>
-                </FormItem>
-              )}
-            />
+          <FormField
+            control={passwordForm.control}
+            name="remember"
+            render={({ field }) => (
+              <FormItem className="flex items-center">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel className="text-content-muted">
+                  Remember me on this device
+                </FormLabel>
+              </FormItem>
+            )}
+          />
 
-            <Button type="submit" size="lg" className="w-full">
-              Sign in
-            </Button>
-          </form>
-        </Form>
-      </section>
-    </div>
+          <Button type="submit" size="lg" className="w-full">
+            Sign in
+          </Button>
+        </form>
+      </Form>
+    </section>
   )
 }

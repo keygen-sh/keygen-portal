885faa4f9c36bdec05be92ffbb15034612cfcd82
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-28 03:15:59 -0500

    add reset password page component

---
+++ src/pages/auth/Reset.tsx
@@ -0,0 +1,79 @@
+import { useNavigate } from "@tanstack/react-router"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/assets/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/assets/components/ui/form"
+import { Input } from "@/assets/components/ui/input"
+
+const resetPasswordSchema = z.object({
+  password: z.string().min(8, "Password must be at least 8 characters."),
+})
+
+export default function Reset() {
+  const navigate = useNavigate()
+
+  const resetPasswordForm = useForm<z.infer<typeof resetPasswordSchema>>({
+    resolver: zodResolver(resetPasswordSchema),
+    defaultValues: {
+      password: "",
+    },
+  })
+
+  function onSubmitResetPassword() {
+    // TODO: Handle previous password case & auth
+    void navigate({ to: "/app/home" })
+  }
+
+  return (
+    <section className="flex w-80 flex-col justify-center">
+      <Form {...resetPasswordForm}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            void resetPasswordForm.handleSubmit(onSubmitResetPassword)(e)
+          }}
+          className="my-3 space-y-7"
+        >
+          <h1 className="bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent select-none">
+            Set a new password
+          </h1>
+          <FormField
+            control={resetPasswordForm.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-content-muted">
+                  New Password
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    toggle={true}
+                    autoComplete="new-password"
+                    autoFocus
+                    placeholder="Enter your password..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" size="lg" className="w-full">
+            Confirm & sign-in
+          </Button>
+        </form>
+      </Form>
+    </section>
+  )
+}

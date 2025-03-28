02a294ef8eed01d600271d01391a3cbca5fc7bea
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-28 03:15:49 -0500

    add recovery email sent page component

---
+++ src/pages/auth/Sent.tsx
@@ -0,0 +1,21 @@
+import BackButton from "@components/BackButton"
+
+export default function Sent() {
+  return (
+    <section className="flex w-80 flex-col justify-center">
+      <div className="flex flex-col space-y-4">
+        <BackButton
+          path="/auth/login"
+          label="Return to Login"
+          className="justify-start md:hidden"
+        />
+        <h1 className="bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent select-none">
+          Check your inbox
+        </h1>
+        <p className="text-sm text-content-muted">
+          We sent you a link to set a new password.
+        </p>
+      </div>
+    </section>
+  )
+}

826ad85bbc0b9fd4325aae7fb6746320276c82c7
Cazden Gabrielse <cgabrielse@outlook.com>
2025-03-31 05:31:32 -0500

    add external sign-on page component

---
+++ src/pages/auth/External.tsx
@@ -0,0 +1,98 @@
+import { useNavigate, Link } from "@tanstack/react-router"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+
+import { Button } from "@/assets/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/assets/components/ui/form"
+import { Input } from "@/assets/components/ui/input"
+
+const externalSchema = z.object({
+  username: z.string().email("Please enter a valid email."),
+})
+
+export default function External() {
+  const navigate = useNavigate()
+
+  const externalForm = useForm<z.infer<typeof externalSchema>>({
+    resolver: zodResolver(externalSchema),
+    defaultValues: { username: "" },
+  })
+
+  function onSubmitExternal() {
+    // TODO: Handle SSO
+    void navigate({ to: "/app/home" })
+  }
+
+  return (
+    <section className="flex w-80 flex-col justify-center">
+      <Form {...externalForm}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            void externalForm.handleSubmit(onSubmitExternal)(e)
+          }}
+          noValidate
+          className="my-3 w-full space-y-7"
+        >
+          <h1 className="bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent select-none">
+            Sign in with SSO
+          </h1>
+          <FormField
+            control={externalForm.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-content-muted">
+                  Email address
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    autoComplete="username"
+                    placeholder="Enter email..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" size="lg" className="w-full">
+            Continue
+          </Button>
+        </form>
+      </Form>
+
+      <div className="space-x-2 rounded border border-content-subdued p-2 text-center text-sm select-none">
+        <span className="text-content-subdued">No account yet?</span>
+        <Button
+          asChild
+          variant="link"
+          size="link"
+          className="text-content-loud"
+        >
+          <Link
+            to="/auth/register"
+            className="text-content-main underline-slide py-0.5 font-bold"
+          >
+            Create one
+          </Link>
+        </Button>
+      </div>
+
+      <div className="mt-2 flex w-full justify-center select-none">
+        <Button variant="link" size="link" asChild>
+          <Link to="/auth/login">Use password instead</Link>
+        </Button>
+      </div>
+    </section>
+  )
+}

463f7a5f03f6d610a36d1f3b7574a7c6e2e8a9de
Cazden Gabrielse <cgabrielse@outlook.com>
2025-04-22 18:11:07 -0500

    add dynamic routing for auth routes

--- src/routes/auth.$id.verify.tsx
+++ src/routes/auth.$id.verify.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router"
 import * as Page from "@pages/index"
 
-export const Route = createFileRoute("/auth/verify")({
+export const Route = createFileRoute("/auth/$id/verify")({
   component: () => <Page.Auth.Verify />,
 })

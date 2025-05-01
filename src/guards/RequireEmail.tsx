ff9eed1211ef580b7478377c2eda55fd8e832564
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-01 03:10:58 -0500

    add render guard components for auth state

---
+++ src/guards/RequireEmail.tsx
@@ -0,0 +1,20 @@
+import { Navigate } from "@tanstack/react-router"
+import { useAuth } from "@hooks/useAuth"
+
+import * as keygen from "@keygen/index"
+
+export default function RequireEmail({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const auth = useAuth()
+
+  if (!auth.email) {
+    console.warn("No email stored in context. Redirecting to login.")
+
+    return Navigate({ to: "/$id/auth/login", params: { id: keygen.config.id } })
+  }
+
+  return <>{children}</>
+}
---
+++ src/guards/index.ts
@@ -0,0 +1,2 @@
+export { default as RequireEmail } from "./RequireEmail"
+export { default as RequirePassword } from "./RequirePassword"

a7e284a864e7f824278f6ec96b203b67d8392a7b
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-01 05:04:53 -0500

    add api token verification to redirect component

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -2,18 +2,31 @@ import { useEffect } from "react"
 import { useNavigate } from "@tanstack/react-router"
 
 import * as keygen from "@keygen/index"
+import { useAuth } from "@hooks/useAuth"
+import * as Loading from "@components/Loading"
 
 export default function Redirect() {
+  const auth = useAuth()
   const navigate = useNavigate()
-  const user = false // dummy
+
+  const tokenId: string | null = localStorage.getItem("tokenId")
+  const token: string | null = localStorage.getItem("token")
+  const user = tokenId && token ? keygen.verify({ tokenId, token }) : null
 
   useEffect(() => {
     if (user) {
+      if (auth.email || auth.password) {
+        auth.clearCredentials()
+      }
       void navigate({ to: "/$id/app/home", params: { id: keygen.config.id } })
     } else {
       void navigate({ to: "/$id/auth/login", params: { id: keygen.config.id } })
     }
-  }, [user, navigate])
+  }, [user, auth, navigate])
 
-  return null
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Loading.Dots />
+    </div>
+  )
 }

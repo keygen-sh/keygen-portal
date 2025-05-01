e5d1360e7b344b2317f2147b31f26aa20bf7d510
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-01 03:11:28 -0500

    add render guards to auth routes

--- src/routes/$id/auth.password.tsx
+++ src/routes/$id/auth.password.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router"
 import * as Page from "@pages/index"
+import * as Guard from "@guards/index"
 
 export const Route = createFileRoute("/$id/auth/password")({
-  component: () => <Page.Auth.Password />,
+  component: () => {
+    return (
+      <Guard.RequireEmail>
+        <Page.Auth.Password />
+      </Guard.RequireEmail>
+    )
+  },
 })

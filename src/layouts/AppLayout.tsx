3f885af8f50b698a6656479ded52cae9555eaff3
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-05 17:08:40 -0500

    remove app layout header

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,9 +3,6 @@ import { Outlet } from "@tanstack/react-router"
 export default function AppLayout() {
   return (
     <main>
-      <div>
-        <h1>App Layout</h1>
-      </div>
       <Outlet />
     </main>
   )

6690aca893b3937ecf1c0502419425f94e0bb431
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-05 17:17:45 -0500

    add sign out button for testing

--- src/pages/app/Dashboard.tsx
+++ src/pages/app/Dashboard.tsx
@@ -1,11 +1,21 @@
+import { Button } from "@/assets/components/ui/button"
+
 export default function App() {
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem("token") || sessionStorage.getItem("token")
+
+  const signOut = () => {
+    localStorage.removeItem("token")
+    sessionStorage.removeItem("token")
+    window.location.href = "/"
+  }
 
   console.log(token)
 
   return (
-    <div>
+    <div className="mt-8 flex flex-col items-center justify-center space-y-8">
       <h1>Hello from "/app/dashboard"!</h1>
+
+      <Button onClick={signOut}>Sign Out</Button>
     </div>
   )
 }

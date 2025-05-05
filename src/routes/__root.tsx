6f750d5b997aacc6862c4db515a73504e13038f3
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-05 18:52:39 -0500

    update css import with alias

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/router-devtools"
-import "../index.css"
+import "@/index.css"
 
 import * as Layout from "@/layouts/index"
 

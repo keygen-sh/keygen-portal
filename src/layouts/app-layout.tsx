96282be3022fae2830ad6a323cc821ed91cd8e5f
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-12 18:36:40 -0500

    add app sidebar to app layout

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "@tanstack/react-router"
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 export default function AppLayout() {
   return (
-    <main>
-      <Outlet />
-    </main>
+    <SidebarProvider>
+      <div className="flex h-screen w-screen overflow-hidden">
+        <AppSidebar />
+        <main className="flex-1 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </SidebarProvider>
   )
 }

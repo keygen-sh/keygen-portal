785841901268811b504fb1b47f3c174f38d06f75
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-17 03:07:37 -0500

    remove tooltip arrow component

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -52,7 +52,6 @@ function TooltipContent({
         {...props}
       >
         {children}
-        <TooltipPrimitive.Arrow className="z-50 size-2.5 translate-y-[calc(-50%_-_2px)] rotate-45 rounded-[2px] bg-primary fill-primary" />
       </TooltipPrimitive.Content>
     </TooltipPrimitive.Portal>
   )

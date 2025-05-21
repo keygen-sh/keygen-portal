84d357d738d67045bf0a98611fd55fc0330daf65
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-20 23:30:59 -0500

    fix layout issue when using provider in root

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,10 +1,5 @@
 import { Outlet } from "@tanstack/react-router"
-import { SidebarProvider } from "@/components/ui/sidebar"
 
 export default function RootLayout() {
-  return (
-    <SidebarProvider>
-      <Outlet />
-    </SidebarProvider>
-  )
+  return <Outlet />
 }

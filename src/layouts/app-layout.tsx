8ce747b8344aee5d832aa2903c3b6f261458dae8
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-20 23:30:59 -0500

    fix layout issue when using provider in root

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -3,35 +3,49 @@ import { useSidebar } from "@/components/ui/sidebar"
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
 
-import { SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 
 import * as Sidebar from "@/components/sidebar"
 
+/**
+ * Render provider first then delegate the rest of the component,
+ * so we can call hooks without violating rules and pushing the provider further up the tree.
+ */
 export default function AppLayout() {
+  return (
+    <SidebarProvider>
+      <AppLayoutContent />
+    </SidebarProvider>
+  )
+}
+
+function AppLayoutContent() {
   const { open } = useSidebar()
   const isMobile = useIsMobile()
 
   return (
-    <div className="flex h-screen w-screen overflow-hidden">
-      <Sidebar.Panel />
-      {isMobile && (
-        <SidebarTrigger
-          className={cn(
-            "absolute top-4 left-4 z-50 rounded-full p-2 transition-opacity duration-300",
-            open
-              ? "pointer-events-none opacity-0"
-              : "pointer-events-auto opacity-100 delay-200",
-          )}
-        />
-      )}
-      <main
-        className={cn(
-          "flex-1 overflow-auto transition-all duration-200",
-          isMobile && open && "pointer-events-none blur-xs",
+    <SidebarProvider>
+      <div className="flex h-screen w-screen overflow-hidden">
+        <Sidebar.Panel />
+        {isMobile && (
+          <SidebarTrigger
+            className={cn(
+              "absolute top-4 left-4 z-50 rounded-full p-2 transition-opacity duration-300",
+              open
+                ? "pointer-events-none opacity-0"
+                : "pointer-events-auto opacity-100 delay-200",
+            )}
+          />
         )}
-      >
-        <Outlet />
-      </main>
-    </div>
+        <main
+          className={cn(
+            "flex-1 overflow-auto transition-all duration-200",
+            isMobile && open && "pointer-events-none blur-xs",
+          )}
+        >
+          <Outlet />
+        </main>
+      </div>
+    </SidebarProvider>
   )
 }

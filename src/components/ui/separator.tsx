1550d2fb68b2d1bfaad7631ee1e9afe5c41f299a
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-29 06:56:14 -0500

    add dashed separator variant

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -8,16 +8,21 @@ import { cn } from "@/lib/utils"
 function Separator({
   className,
   orientation = "horizontal",
+  dashed = false,
   decorative = true,
   ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+}: React.ComponentProps<typeof SeparatorPrimitive.Root> & {
+  dashed?: boolean
+}) {
   return (
     <SeparatorPrimitive.Root
       data-slot="separator-root"
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+        dashed
+          ? "shrink-0 border border-dashed border-border data-[orientation=horizontal]:w-full data-[orientation=horizontal]:border-t data-[orientation=vertical]:h-full data-[orientation=vertical]:border-l"
+          : "shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
         className,
       )}
       {...props}

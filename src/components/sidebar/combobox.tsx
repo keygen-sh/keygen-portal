44b36471fc7ef3215c032ada7296f6ee7c61f313
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-29 07:06:10 -0500

    add environments modal to sidebar

--- src/components/sidebar/combobox.tsx
+++ src/components/sidebar/combobox.tsx
@@ -19,6 +19,8 @@ import {
 
 import { Droplet } from "lucide-react"
 
+import * as Environments from "@/components/environments"
+
 type Env = { id: string; label: string }
 type Prod = { id: string; name: string; icon: string | LucideIcon; envs: Env[] }
 
@@ -54,6 +56,8 @@ export default function SidebarCombobox(): React.ReactElement {
   const [productId, setProductId] = useState(products[0].id)
   const [envId, setEnvId] = useState(products[0].envs[0].id)
 
+  const [showEnvironmentsModal, setShowEnvironmentsModal] = useState(false)
+
   const currentProduct = useMemo(
     () => products.find((p) => p.id === productId)!,
     [productId],
@@ -61,68 +65,85 @@ export default function SidebarCombobox(): React.ReactElement {
   const currentEnvironment = currentProduct.envs.find((e) => e.id === envId)!
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="ghost"
-          aria-expanded={open}
-          size="default"
-          className="!px-1 !py-0"
-        >
-          {typeof currentProduct.icon === "string" ? (
-            <img src={currentProduct.icon} />
-          ) : (
-            <currentProduct.icon className="mr-2 size-6 rounded-sm bg-content-loud p-1 text-background" />
-          )}
-          <div className="flex max-w-32 flex-col text-left text-content-loud">
-            <div className="flex items-center gap-2">
-              <span className="truncate">{currentProduct.name}</span>
-              <ChevronsUpDown className="size-3 opacity-60" />
+    <>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="ghost"
+            aria-expanded={open}
+            size="default"
+            className="!px-1 !py-0"
+          >
+            {typeof currentProduct.icon === "string" ? (
+              <img src={currentProduct.icon} />
+            ) : (
+              <currentProduct.icon className="mr-2 size-6 rounded-sm bg-content-loud p-1 text-background" />
+            )}
+            <div className="flex max-w-32 flex-col text-left text-content-loud">
+              <div className="flex items-center gap-2">
+                <span className="truncate">{currentProduct.name}</span>
+                <ChevronsUpDown className="size-3 opacity-60" />
+              </div>
+              <span className="text-xs font-normal text-content-normal">
+                {currentEnvironment.label}
+              </span>
             </div>
-            <span className="text-xs font-normal text-content-normal">
-              {currentEnvironment.label}
-            </span>
-          </div>
-        </Button>
-      </PopoverTrigger>
+          </Button>
+        </PopoverTrigger>
 
-      <PopoverContent className="mt-1 w-60 p-0">
-        <Command>
-          <CommandInput placeholder="Search..." />
-          <CommandList>
-            <CommandEmpty>No match.</CommandEmpty>
+        <PopoverContent className="mt-1 w-60 p-0">
+          <Command>
+            <CommandInput placeholder="Search..." />
+            <CommandList>
+              <CommandEmpty>No match.</CommandEmpty>
 
-            {products.map((prod) => (
-              <Fragment key={prod.id}>
-                <CommandGroup heading={prod.name}>
-                  {prod.envs.map((env) => {
-                    const selected = prod.id === productId && env.id === envId
-                    return (
-                      <CommandItem
-                        key={env.id}
-                        value={env.id}
-                        onSelect={() => {
-                          setProductId(prod.id)
-                          setEnvId(env.id)
-                          setOpen(false)
-                        }}
-                      >
-                        {selected ? (
-                          <Check className="mr-2 size-4" />
-                        ) : (
-                          <Circle className="mr-2 size-4 opacity-0" />
-                        )}
-                        {env.label}
-                      </CommandItem>
-                    )
-                  })}
-                </CommandGroup>
-                <CommandSeparator />
-              </Fragment>
-            ))}
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+              {products.map((prod) => (
+                <Fragment key={prod.id}>
+                  <CommandGroup heading={prod.name}>
+                    {prod.envs.map((env) => {
+                      const selected = prod.id === productId && env.id === envId
+                      return (
+                        <CommandItem
+                          key={env.id}
+                          value={env.id}
+                          onSelect={() => {
+                            setProductId(prod.id)
+                            setEnvId(env.id)
+                            setOpen(false)
+                          }}
+                        >
+                          {selected ? (
+                            <Check className="mr-2 size-4" />
+                          ) : (
+                            <Circle className="mr-2 size-4 opacity-0" />
+                          )}
+                          {env.label}
+                        </CommandItem>
+                      )
+                    })}
+                  </CommandGroup>
+                  <CommandSeparator />
+                </Fragment>
+              ))}
+
+              <CommandItem
+                onSelect={() => {
+                  setShowEnvironmentsModal(true)
+                  setOpen(false)
+                }}
+                className="m-1 text-content-normal"
+              >
+                Manage Environments
+              </CommandItem>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+
+      <Environments.Modal
+        open={showEnvironmentsModal}
+        onClose={() => setShowEnvironmentsModal(false)}
+      />
+    </>
   )
 }

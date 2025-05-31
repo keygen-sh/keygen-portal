836adb23efb00646ce464c75ed48ff698bdb817f
Cazden Gabrielse <cgabrielse@outlook.com>
2025-05-31 01:59:51 -0500

    add fallback when no environments exist

--- src/components/environments/view/list.tsx
+++ src/components/environments/view/list.tsx
@@ -20,28 +20,34 @@ export default function EnvironmentsList({
 }: EnvironmentsListProps) {
   return (
     <div className="flex flex-grow flex-col justify-between px-4">
-      <Table>
-        <TableHeader>
-          <TableRow className="pointer-events-none">
-            <TableHead className="w-48">Name</TableHead>
-            <TableHead className="w-12">Code</TableHead>
-            <TableHead className="w-32">Isolation Strategy</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {data.map((env) => (
-            <TableRow
-              key={env.id}
-              onClick={() => onViewDetails(env)}
-              className="cursor-pointer"
-            >
-              <TableCell className="font-medium">{env.name}</TableCell>
-              <TableCell>{env.code}</TableCell>
-              <TableCell>{env.isolationStrategy}</TableCell>
+      {data && data.length > 0 ? (
+        <Table>
+          <TableHeader>
+            <TableRow className="pointer-events-none">
+              <TableHead className="w-48">Name</TableHead>
+              <TableHead className="w-12">Code</TableHead>
+              <TableHead className="w-32">Isolation Strategy</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {data.map((env) => (
+              <TableRow
+                key={env.id}
+                onClick={() => onViewDetails(env)}
+                className="cursor-pointer"
+              >
+                <TableCell className="font-medium">{env.name}</TableCell>
+                <TableCell>{env.code}</TableCell>
+                <TableCell>{env.isolationStrategy}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <p className="my-8 text-center text-sm text-content-subdued">
+          Looks empty. Create an environment to get started.
+        </p>
+      )}
     </div>
   )
 }

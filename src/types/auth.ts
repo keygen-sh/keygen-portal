726f223b341b40bda6debc482a01ef22c2643f22
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-02 15:37:08 -0500

    add auth types

---
+++ src/types/auth.ts
@@ -0,0 +1,53 @@
+import { APIResponse } from "@/types/api"
+
+export interface AuthAttributes {
+  token: string
+  kind: string
+  name: string | null
+  permissions: string[]
+  expiry: string | null
+  created: string
+  updated: string
+}
+
+export interface AuthRelationships {
+  account: {
+    links: {
+      related: string
+    }
+    data: {
+      id: string
+      type: "accounts"
+    }
+  }
+  bearer: {
+    links: {
+      related: string
+    }
+    data: {
+      id: string
+      type: "users"
+    }
+  }
+  environment: {
+    links: {
+      related: string | null
+    }
+    data: {
+      id: string
+      type: "environments"
+    } | null
+  }
+}
+
+export interface Auth {
+  id: string
+  type: "tokens"
+  attributes: AuthAttributes
+  relationships: AuthRelationships
+  links: {
+    self: string
+  }
+}
+
+export type AuthResponse = APIResponse<Auth>

6ca6dce6877c5ee86e232f9db885644306abe26b
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-05 13:12:07 -0500

    refactor index route to use tanstack's redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,16 @@
-import { createFileRoute } from "@tanstack/react-router"
-import * as Page from "@/pages/index"
+import { createFileRoute, redirect } from "@tanstack/react-router"
+import * as keygen from "@/keygen"
 
 export const Route = createFileRoute("/")({
-  component: () => <Page.Redirect />,
+  loader: () => {
+    const token =
+      localStorage.getItem("token") ?? sessionStorage.getItem("token")
+
+    return redirect({
+      to: token ? "/$id/app/dashboard" : "/$id/auth/login",
+      params: { id: keygen.config.id },
+      replace: true,
+    })
+  },
+  component: () => null,
 })

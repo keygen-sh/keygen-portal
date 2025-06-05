71fabaa713f648d84df89dd545590afbb2305541
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-05 13:09:38 -0500

    add session hook to app layout

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "@tanstack/react-router"
+import { useSession } from "@/hooks/use-session"
 import { useSidebar } from "@/components/ui/sidebar"
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -7,12 +8,23 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { Toaster } from "@/components/ui/sonner"
 
 import * as Sidebar from "@/components/sidebar"
+import * as Loading from "@/components/loading"
 
 /**
  * Render provider first then delegate the rest of the component,
  * so we can call hooks without violating rules and pushing the provider further up the tree.
  */
 export default function AppLayout() {
+  const { initializing } = useSession()
+
+  if (initializing) {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <Loading.Dots />
+      </div>
+    )
+  }
+
   return (
     <SidebarProvider>
       <AppLayoutContent />

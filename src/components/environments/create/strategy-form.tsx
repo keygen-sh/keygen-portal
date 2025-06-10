199305aecd9fa37749cf0e5377af9523359c4cd1
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-10 00:42:08 -0500

    refactor form height to fit form errors

--- src/components/environments/create/strategy-form.tsx
+++ src/components/environments/create/strategy-form.tsx
@@ -69,7 +69,7 @@ export default function StrategyForm({
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <ScrollArea className="h-[60vh] md:h-[25vh]">
+        <ScrollArea className="h-[60vh] md:h-[30vh]">
           <div className="flex h-full flex-col justify-between p-4">
             <div className="mb-6 flex items-center gap-2">
               <h2 className="text-xl font-semibold md:text-lg">

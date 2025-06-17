0ba8b181b1e2b553322bcf4a99b31ec2c7308758
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-16 23:22:12 -0500

    refactor sonner for custom toast component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,23 +1,20 @@
 import { useTheme } from "next-themes"
 import { Toaster as Sonner, ToasterProps } from "sonner"
 
-const Toaster = ({ ...props }: ToasterProps) => {
+export const Toaster = (props: ToasterProps) => {
   const { theme = "system" } = useTheme()
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      toastOptions={{
+        classNames: {
+          toast: "flex w-full justify-center",
+        },
+      }}
       className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
+      position="top-center"
       {...props}
     />
   )
 }
-
-export { Toaster }

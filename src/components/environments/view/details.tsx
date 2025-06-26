552ca98a744ad22cd0f4a7ab3fe39b8fc998db48
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-26 07:16:20 -0500

    fix wording in delete environment modal

--- src/components/environments/view/details.tsx
+++ src/components/environments/view/details.tsx
@@ -105,7 +105,7 @@ export default function EnvironmentDetails({
                   {loading ? (
                     <Loading.Dots className="bg-background" />
                   ) : (
-                    "Create"
+                    "Delete"
                   )}
                 </Button>
               </AlertDialogFooter>

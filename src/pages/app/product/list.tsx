283ed872df1eab996681a4ae07e9fd96129f74eb
Cazden Gabrielse <cgabrielse@outlook.com>
2025-06-29 06:41:02 -0500

    refactor products list skeleton spacing

--- src/pages/app/product/list.tsx
+++ src/pages/app/product/list.tsx
@@ -246,12 +246,12 @@ export default function ProductsList() {
           </div>
         </div>
       ) : (
-        <div className="flex w-full flex-col justify-center space-y-2 p-4">
+        <div className="flex w-full flex-col justify-center space-y-4 p-4 pr-12">
           <Skeleton className="mb-4 h-6 w-full" />
-          <Skeleton className="h-8 w-3/4" />
-          <Skeleton className="h-8 w-1/2" />
-          <Skeleton className="h-8 w-1/4" />
-          <Skeleton className="h-8 w-1/3" />
+          <Skeleton className="ml-4 h-8 w-3/4" />
+          <Skeleton className="ml-4 h-8 w-1/2" />
+          <Skeleton className="ml-4 h-8 w-1/4" />
+          <Skeleton className="ml-4 h-8 w-1/3" />
           <div className="flex w-full justify-end">
             <Skeleton className="h-8 w-36" />
           </div>

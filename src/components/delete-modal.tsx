0c8653990fa77ad3f173a1c140e7eee65a67c345
Cazden Gabrielse <cgabrielse@outlook.com>
2025-07-11 12:32:42 -0500

    add delete modal component

---
+++ src/components/delete-modal.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button"
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogCancel,
+} from "@/components/ui/alert-dialog"
+
+import * as Loading from "@/components/loading"
+
+interface DeleteModalProps {
+  title: string
+  description?: string
+  open: boolean
+  disabled?: boolean
+  onClose: () => void
+  onDelete: () => void
+}
+
+export default function DeleteModal({
+  title,
+  description,
+  open,
+  disabled,
+  onClose,
+  onDelete,
+}: DeleteModalProps) {
+  return (
+    <AlertDialog open={open} onOpenChange={onClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader className="text-start">
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter className="gap-4">
+          <AlertDialogCancel asChild>
+            <Button variant="outline" disabled={disabled}>
+              Cancel
+            </Button>
+          </AlertDialogCancel>
+          <Button variant="destructive" disabled={disabled} onClick={onDelete}>
+            {disabled ? <Loading.Dots className="bg-background" /> : "Delete"}
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}

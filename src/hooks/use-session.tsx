1b8082d1d2d9475c9a853f7a4f8ee22fdbd35c67
Cazden Gabrielse <cgabrielse@outlook.com>
2025-07-11 13:01:47 -0500

    fix redirect on page refresh

--- src/hooks/use-session.tsx
+++ src/hooks/use-session.tsx
@@ -1,11 +1,9 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "@tanstack/react-router"
 import * as keygen from "@/keygen"
 
 const STORAGE_KEYS = ["token", "tokenId"]
 
 export function useSession() {
-  const navigate = useNavigate()
   const [initializing, setInitializing] = useState(true)
 
   useEffect(() => {
@@ -25,11 +23,6 @@ export function useSession() {
 
         keygen.client.setToken(token)
 
-        navigate({
-          to: "/$id/app/dashboard",
-          params: { id: keygen.config.id },
-        })
-
         return
       } catch (error) {
         console.error(error)
@@ -40,7 +33,7 @@ export function useSession() {
     }
 
     verify()
-  }, [navigate])
+  }, [])
 
   useEffect(() => {
     function handleStorage(e: StorageEvent) {

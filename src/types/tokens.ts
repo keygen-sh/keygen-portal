0a01e4088cc92dfc020ef0a1841b8131cf921304
Cazden Gabrielse <cgabrielse@outlook.com>
2025-08-04 16:39:18 -0500

    add tokens types file

---
+++ src/types/tokens.ts
@@ -0,0 +1,51 @@
+import {
+  APIResponse,
+  Resource,
+  Relationship,
+  Linkage,
+  Writable,
+} from "@/types/api"
+
+export interface TokenAttributes {
+  kind: string
+  token?: string
+  name?: string | null
+  expiry?: string | null
+  created: string
+  updated: string
+  permissions?: string[] | null
+  maxActivations?: number | null
+  activations?: number | null
+  maxDeactivations?: number | null
+  deactivations?: number | null
+}
+
+export type TokenRelationships = {
+  account: Relationship<Linkage<"accounts">>
+  environment?: Relationship<Linkage<"environments">>
+  bearer?: Relationship<
+    Linkage<"users"> | Linkage<"products"> | Linkage<"licenses">
+  >
+}
+
+export type Token = Resource<"tokens", TokenAttributes, TokenRelationships>
+
+export type TokenResponse = APIResponse<Token>
+export type TokensListResponse = APIResponse<Token[]>
+
+export type CreateTokenPayload =
+  | {
+      environment: Exclude<TokenRelationships["environment"], undefined>
+      bearer?: never
+    }
+  | {
+      bearer: Exclude<TokenRelationships["bearer"], undefined>
+      environment?: never
+    }
+
+export interface CreateTokenVariables {
+  relationships: CreateTokenPayload
+  cacheKey?: (string | number)[]
+}
+
+export type UpdateTokenPayload = Partial<Writable<TokenAttributes>>

aae224fdbe2a1f2a1ce64e73268c3deb00e97b74
Cazden Gabrielse <cgabrielse@outlook.com>
2025-08-26 23:54:50 -0500

    add class name prop to property section

--- src/components/property/section.tsx
+++ src/components/property/section.tsx
@@ -1,14 +1,18 @@
+import { cn } from "@/lib/utils"
+
 interface PropertySectionProps {
   title: string
   children?: React.ReactNode
+  className?: string
 }
 
 export default function PropertySection({
   title,
   children,
+  className,
 }: PropertySectionProps): React.ReactElement {
   return (
-    <section className="w-full">
+    <section className={cn("w-full", className)}>
       <h3 className="text-xs font-semibold text-content-muted">{title}</h3>
       <div className="flex flex-col gap-3 pt-3">{children}</div>
     </section>

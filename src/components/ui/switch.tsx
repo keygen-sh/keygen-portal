a08f36610300457c4e17004839f092f563c4a899
Cazden Gabrielse <cgabrielse@outlook.com>
2025-09-09 05:56:50 -0500

    update switch component base styles

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -11,15 +11,15 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
-        className
+        "peer relative inline-flex h-[var(--h)] w-8 shrink-0 items-center rounded-full border border-transparent p-[var(--p)] shadow-xs transition-all outline-none [--h:1.15rem] [--p:2px] focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-background-3 dark:data-[state=unchecked]:bg-input/80",
+        className,
       )}
       {...props}
     >
       <SwitchPrimitive.Thumb
         data-slot="switch-thumb"
         className={cn(
-          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
+          "pointer-events-none absolute top-1/2 left-[var(--p)] size-[calc(var(--h)-2*var(--p))] -translate-y-1/2 rounded-full ring-0 transition-all will-change-transform data-[state=checked]:left-[calc(100%-var(--p))] data-[state=checked]:-translate-x-full data-[state=checked]:bg-background data-[state=unchecked]:bg-content-disabled dark:data-[state=checked]:bg-primary-foreground dark:data-[state=unchecked]:bg-foreground",
         )}
       />
     </SwitchPrimitive.Root>

48c4202651e2e1fcccac6d493ff11fccdf7eb5e1
Cazden Gabrielse <cgabrielse@outlook.com>
2025-09-09 07:02:07 -0500

    refactor policies lib with enums

--- src/lib/policies.tsx
+++ src/lib/policies.tsx
@@ -6,6 +6,7 @@ import {
   Policy,
   PolicyDurations,
   PolicyAttributeDescriptions,
+  ExpirationStrategy,
 } from "@/types/policies"
 
 export function isPerpetual(policy: Policy): boolean {
@@ -13,36 +14,40 @@ export function isPerpetual(policy: Policy): boolean {
 }
 
 export function isTimed(policy: Policy): boolean {
-  const d = policy.attributes.duration
-  return typeof d === "number" && d > 0
+  const duration = policy.attributes.duration
+
+  return typeof duration === "number" && duration > 0
 }
 
 export function isPerpetualFallback(policy: Policy): boolean {
   return (
     isTimed(policy) &&
-    policy.attributes.expirationStrategy === "MAINTAIN_ACCESS"
+    policy.attributes.expirationStrategy === ExpirationStrategy.MAINTAIN_ACCESS
   )
 }
 
 export function isNodeLocked(policy: Policy): boolean {
-  const m = policy.attributes.maxMachines
+  const maxMachines = policy.attributes.maxMachines
+
   return (
-    (typeof m === "number" && m > 0) ||
+    (typeof maxMachines === "number" && maxMachines > 0) ||
     policy.attributes.requireFingerprintScope === true
   )
 }
 
 export function isUserLocked(policy: Policy): boolean {
-  const u = policy.attributes.maxUsers
+  const maxUsers = policy.attributes.maxUsers
+
   return (
-    (typeof u === "number" && u > 0) ||
+    (typeof maxUsers === "number" && maxUsers > 0) ||
     policy.attributes.requireUserScope === true
   )
 }
 
 export function isProcessBased(policy: Policy): boolean {
-  const p = policy.attributes.maxProcesses
-  return typeof p === "number" && p > 0
+  const maxProcesses = policy.attributes.maxProcesses
+
+  return typeof maxProcesses === "number" && maxProcesses > 0
 }
 
 export function isLeaseBased(policy: Policy): boolean {
@@ -51,12 +56,14 @@ export function isLeaseBased(policy: Policy): boolean {
 
 export function isFeatureBased(policy: Policy): boolean {
   const count = policy.relationships?.entitlements?.data?.length ?? 0
+
   return count > 0
 }
 
 export function isUsageBased(policy: Policy): boolean {
-  const uses = policy.attributes.maxUses
-  return typeof uses === "number" && uses > 0
+  const maxUses = policy.attributes.maxUses
+
+  return typeof maxUses === "number" && maxUses > 0
 }
 
 export function policyDurationToString(seconds?: number | null): string {

b5c8ef748b9f248e402316f71252e4e0e267c26c
Cazden Gabrielse <cgabrielse@outlook.com>
2025-09-24 22:25:55 -0500

    add variants to field header component

--- src/components/field/header.tsx
+++ src/components/field/header.tsx
@@ -11,26 +11,40 @@ import {
 
 import { Info } from "lucide-react"
 
+import { cn } from "@/lib/utils"
+
 import { useMobile } from "@/hooks/use-mobile"
+
+type FieldHeaderVariant = "row" | "stacking"
 
 interface FieldHeaderProps {
   label: string
-  tooltip: string
+  tooltip?: string | null
+  variant?: FieldHeaderVariant
   children: React.ReactNode
+  className?: string
 }
 
 export default function FieldHeader({
   label,
-  tooltip,
+  tooltip = null,
+  variant = "row",
   children,
+  className,
 }: FieldHeaderProps) {
   const isMobile = useMobile()
 
   return (
-    <div className="flex w-full flex-col gap-2 md:flex-row md:items-center md:justify-between">
+    <div
+      className={cn(
+        "flex w-full flex-col gap-2",
+        variant === "row" && "md:flex-row md:items-center md:justify-between",
+        className,
+      )}
+    >
       <div className="flex items-center gap-2">
         <label className="text-sm text-content-muted">{label}</label>
-        {isMobile ? (
+        {isMobile && tooltip ? (
           <Popover>
             <PopoverTrigger onClick={(e) => e.stopPropagation()}>
               <Info className="size-5 text-content-subdued" />
@@ -40,17 +54,23 @@ export default function FieldHeader({
             </PopoverContent>
           </Popover>
         ) : (
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <Info className="size-4 pt-0.5 text-content-subdued" />
-            </TooltipTrigger>
-            <TooltipContent className="max-w-80 bg-background-4 text-pretty text-content-muted">
-              {tooltip}
-            </TooltipContent>
-          </Tooltip>
+          tooltip && (
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Info className="size-4 pt-0.5 text-content-subdued" />
+              </TooltipTrigger>
+              <TooltipContent className="max-w-80 bg-background-4 text-pretty text-content-muted">
+                {tooltip}
+              </TooltipContent>
+            </Tooltip>
+          )
         )}
       </div>
-      <div className="w-full md:max-w-48 md:min-w-48">{children}</div>
+      <div
+        className={cn("w-full", variant === "row" && "md:max-w-48 md:min-w-48")}
+      >
+        {children}
+      </div>
     </div>
   )
 }

4cbbdbad98437c3b9bdc5917e8079e6bec004a0f
Cazden Gabrielse <cgabrielse@outlook.com>
2025-09-26 07:03:12 -0500

    add initial entitlements types

--- src/types/entitlements.ts
+++ src/types/entitlements.ts
@@ -1,4 +1,26 @@
-import { Resource, Relationship, Linkage } from "@/types/api"
+import {
+  APIResponse,
+  Resource,
+  Relationship,
+  Linkage,
+  Writable,
+  OptionalExcept,
+} from "@/types/api"
+
+export enum EntitlementErrorCode {
+  CODE_TAKEN = "CODE_TAKEN",
+}
+
+export enum EntitlementMode {
+  VIEW = "view",
+  EDIT = "edit",
+  CREATE = "create",
+}
+
+export enum EntitlementView {
+  LIST = "list",
+  DETAILS = "details",
+}
 
 export interface EntitlementAttributes {
   name: string
@@ -17,6 +39,15 @@ export type Entitlement = Resource<
   EntitlementAttributes,
   EntitlementRelationships
 >
+
+export type EntitlementResponse = APIResponse<Entitlement>
+export type EntitlementListResponse = APIResponse<Entitlement[]>
+
+export type CreateEntitlementPayload = OptionalExcept<
+  Writable<EntitlementAttributes>,
+  "name" & "code"
+>
+export type UpdateEntitlementPayload = Partial<Writable<EntitlementAttributes>>
 
 export const MockEntitlements: Entitlement[] = [
   {
@@ -45,6 +76,28 @@ export const MockEntitlements: Entitlement[] = [
     },
   },
   {
+    id: "kb23jkb-f42f-4623-952b-ca7f2600bded",
+    type: "entitlements",
+    attributes: {
+      name: "Example Feature 2",
+      code: "EXAMPLE_FEATURE_2",
+      metadata: {},
+      created: "2017-01-02T20:26:53.464Z",
+      updated: "2017-01-02T20:26:53.464Z",
+    },
+    relationships: {
+      account: {
+        links: {
+          related: "/v1/accounts/<account>",
+        },
+        data: {
+          type: "accounts",
+          id: "<account>",
+        },
+      },
+    },
+    links: {
+      self: "/v1/accounts/<account>/entitlements/kb23jkb-f42f-4623-952b-ca7f2600bded",
     id: "kb23jkb-f42f-4623-952b-ca7f2600bded",
     type: "entitlements",
     attributes: {

f145a5ae300deec7c565b7efd537d49c546b26ec
Zeke Gabrielse <ezekg@yahoo.com>
2025-11-20 15:25:06 -0600

    refactor mutation results to use partitioning

---
+++ src/lib/partition.ts
@@ -0,0 +1,23 @@
+// partition primitive to partition an array by a predicate function
+export type Partition<T> = [T[], T[]]
+
+export function partition<T>(values: T[], predicateFn: (v: T) => boolean): Partition<T> {
+  return values.reduce(
+    ([x, y], v): Partition<T> => (predicateFn(v) ? [[...x, v], y] : [x, [...y, v]]),
+    [[], []] as Partition<T>,
+  )
+}
+
+// partition helper for Promise.allSettled result
+export type PromiseRejectedResultWithReason<T> = Omit<PromiseRejectedResult, 'reason'> & { reason: T }
+
+export type PromiseSettledPartition<FulfilledValue, RejectedReason = unknown> = [
+  PromiseFulfilledResult<FulfilledValue>[],
+  PromiseRejectedResultWithReason<RejectedReason>[],
+]
+
+export function partitionFulfilled<FulfilledValue, RejectedReason>(
+  results: PromiseSettledResult<FulfilledValue>[],
+): PromiseSettledPartition<FulfilledValue, RejectedReason> {
+  return partition(results, r => r.status === 'fulfilled') as PromiseSettledPartition<FulfilledValue, RejectedReason>
+}

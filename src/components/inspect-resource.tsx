10396455c909387f1e9b68c446e0e5b37f93fa56
Cazden Gabrielse <cgabrielse@outlook.com>
2026-01-07 15:29:24 -0600

    add inspect resource component

---
+++ src/components/inspect-resource.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+import { Copy } from "lucide-react"
+
+import { Policy } from "@/types/policies"
+import { Product } from "@/types/products"
+import { License } from "@/types/licenses"
+import { Entitlement } from "@/types/entitlements"
+
+import { cn } from "@/lib/utils"
+import { copyToClipboard } from "@/lib/clipboard"
+
+interface InspectResourceProps {
+  resource: Policy | Product | License | Entitlement
+  className?: string
+}
+
+export default function InspectResource({
+  resource,
+  className,
+}: InspectResourceProps): React.ReactElement {
+  return (
+    <div
+      className={cn(
+        "relative m-2 min-h-0 min-w-0 flex-1 overflow-hidden rounded border border-accent",
+        className,
+      )}
+    >
+      <Button
+        variant="ghost"
+        size="icon"
+        onClick={() => copyToClipboard(JSON.stringify(resource, null, 2))}
+        className="absolute top-3 right-3 z-10 h-7 w-7 bg-accent/60 md:bg-accent/0"
+      >
+        <Copy className="size-3.5" />
+      </Button>
+
+      {/* FIXME(cazden) Text should be scrollable along X on smaller screens */}
+      <ScrollArea className="size-full">
+        <pre className="w-max min-w-full p-3 font-mono text-sm leading-snug whitespace-pre">
+          {JSON.stringify(resource, null, 2)}
+        </pre>
+      </ScrollArea>
+    </div>
+  )
+}

0f5d655be0473c01fa0201bf1f467fb00471756a
Cazden Gabrielse <cgabrielse@outlook.com>
2026-01-12 13:53:20 -0600

    add groups attribute type schema

---
+++ src/lib/groups.ts
@@ -0,0 +1,13 @@
+import { AttributeType } from "@/components/attribute/value"
+
+import { Group } from "@/types/groups"
+
+export const groupAttributeTypeSchema: Record<
+  keyof Omit<Group["attributes"], "metadata" | "created" | "updated">,
+  AttributeType
+> = {
+  name: "string",
+  maxUsers: "number",
+  maxLicenses: "number",
+  maxMachines: "number",
+}

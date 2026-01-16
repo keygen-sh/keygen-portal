4e55e15a67470ad75926f535d6b36aa77e9a8f13
Cazden Gabrielse <cgabrielse@outlook.com>
2026-01-16 12:54:31 -0600

    add use components table columns hook

---
+++ src/hooks/use-component-table-columns.tsx
@@ -0,0 +1,54 @@
+import { useMemo } from "react"
+
+import { Component } from "@/types/components"
+
+import { createTableColumnHelper } from "@/lib/tables"
+
+import * as Tables from "@/components/tables"
+import ClipboardButton from "@/components/clipboard-button"
+
+const column = createTableColumnHelper<Component>()
+
+export function useComponentTableColumns() {
+  const columns = useMemo(
+    () => [
+      column.id({
+        header: "ID",
+        cell: (info) => <ClipboardButton value={info.getValue()} />,
+      }),
+      column.attr("name", {
+        header: "Name",
+        cell: (info) =>
+          info.getValue() || <span className="text-content-muted">--</span>,
+      }),
+      column.attr("fingerprint", {
+        header: "Fingerprint",
+        cell: (info) =>
+          info.getValue() || <span className="text-content-muted">--</span>,
+      }),
+      column.rel("machine", {
+        sortingFn: "alphanumeric",
+        header: "Machine",
+        cell: (info) => <Tables.MachineCell id={info.getValue()?.data?.id} />,
+      }),
+      column.rel("license", {
+        sortingFn: "alphanumeric",
+        header: "License",
+        cell: (info) => <Tables.LicenseCell id={info.getValue()?.data?.id} />,
+      }),
+      column.attr("created", {
+        sortingFn: "datetime",
+        header: "Created",
+        cell: (info) => new Date(info.getValue()).toLocaleDateString(),
+      }),
+      column.attr("updated", {
+        sortingFn: "datetime",
+        header: "Updated",
+        cell: (info) => new Date(info.getValue()).toLocaleDateString(),
+      }),
+    ],
+    [],
+  )
+
+  return columns
+}

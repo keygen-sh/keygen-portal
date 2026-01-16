eadfc27cf269991c22d61e3907d4d91319b65f29
Cazden Gabrielse <cgabrielse@outlook.com>
2026-01-16 12:59:54 -0600

    add components advanced dialog

--- src/components/components/index.ts
+++ src/components/components/index.ts
@@ -6,3 +6,7 @@ export { Edit }
 
 import * as Fields from "./fields"
 export { Fields }
+
+export { default as AttributeGroup } from "./attribute-group"
+export { default as AllAttributes } from "./all-attributes"
+export { default as AdvancedDialog } from "./advanced-dialog"

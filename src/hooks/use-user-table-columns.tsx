34853068d3d52717365763689d381795938de19b
Cazden Gabrielse <cgabrielse@outlook.com>
2026-01-20 10:40:02 -0600

    add use user table column hook

---
+++ src/hooks/use-user-table-columns.tsx
@@ -0,0 +1,68 @@
+import { useMemo } from "react"
+
+import { Badge } from "@/components/ui/badge"
+
+import {
+  User,
+  UserRoleLabels,
+  UserStatusLabels,
+  UserStatusVariants,
+} from "@/types/users"
+
+import { createTableColumnHelper } from "@/lib/tables"
+
+import ClipboardButton from "@/components/clipboard-button"
+
+const column = createTableColumnHelper<User>()
+
+export function useUserTableColumns() {
+  const columns = useMemo(
+    () => [
+      column.id({
+        header: "ID",
+        cell: (info) => <ClipboardButton value={info.getValue()} />,
+      }),
+      column.attr("fullName", {
+        header: "Name",
+        cell: (info) =>
+          info.getValue() || <span className="text-content-muted">--</span>,
+      }),
+      column.attr("email", {
+        header: "Email",
+        cell: (info) =>
+          info.getValue() || <span className="text-content-muted">--</span>,
+      }),
+      column.attr("status", {
+        header: "Status",
+        cell: (info) => {
+          const status = info.getValue()
+          return (
+            <Badge variant={UserStatusVariants[status]}>
+              {UserStatusLabels[status]}
+            </Badge>
+          )
+        },
+      }),
+      column.attr("role", {
+        header: "Role",
+        cell: (info) => {
+          const role = info.getValue()
+          return UserRoleLabels[role] || role
+        },
+      }),
+      column.attr("created", {
+        sortingFn: "datetime",
+        header: "Created",
+        cell: (info) => new Date(info.getValue()).toLocaleDateString(),
+      }),
+      column.attr("updated", {
+        sortingFn: "datetime",
+        header: "Updated",
+        cell: (info) => new Date(info.getValue()).toLocaleDateString(),
+      }),
+    ],
+    [],
+  )
+
+  return columns
+}

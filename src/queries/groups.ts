2c408d44d70d98ee324e4291fd9f6a458187f7cb
Cazden Gabrielse <cgabrielse@outlook.com>
2026-01-24 20:51:12 -0600

    add groups query hooks

---
+++ src/queries/groups.ts
@@ -0,0 +1,114 @@
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query"
+
+import { useEnvironment } from "@/hooks/use-environment"
+
+import * as Forms from "@/forms"
+
+import { APIError } from "@/types/api"
+import { Group } from "@/types/groups"
+
+import * as keygen from "@/keygen"
+import { diff } from "@/lib/utils"
+
+export function useGetGroup(groupId: string) {
+  const { code } = useEnvironment()
+
+  return useQuery({
+    queryKey: ["groups", groupId, { environment: code }],
+    queryFn: async () => {
+      const response = await keygen.groups.get({ id: groupId })
+
+      if (!response.data) {
+        throw new Error("Group not found")
+      }
+
+      return response.data
+    },
+    retry: (failures, error) =>
+      error.message !== "Group not found" && failures < 3,
+    enabled: !!groupId,
+  })
+}
+
+export function useListGroups() {
+  const { code } = useEnvironment()
+
+  return useQuery({
+    queryKey: ["groups", { environment: code }],
+    queryFn: () =>
+      keygen.groups.list({}).then((response) => response.data ?? []),
+  })
+}
+
+export function useCreateGroup() {
+  const queryClient = useQueryClient()
+  const { code } = useEnvironment()
+
+  return useMutation<Group, APIError, Forms.Groups.CreateValues>({
+    mutationFn: (values) =>
+      keygen.groups.create(values).then((response) => response.data as Group),
+
+    onSuccess: (newGroup) => {
+      queryClient.setQueryData<Group[]>(
+        ["groups", { environment: code }],
+        (old) => (old ? [newGroup, ...old] : [newGroup]),
+      )
+      queryClient.setQueryData(
+        ["groups", newGroup.id, { environment: code }],
+        newGroup,
+      )
+    },
+  })
+}
+
+export function useUpdateGroup(groupId: string) {
+  const queryClient = useQueryClient()
+  const { code } = useEnvironment()
+
+  return useMutation<Group, APIError, Forms.Groups.UpdateValues>({
+    mutationFn: (values) =>
+      keygen.groups.get({ id: groupId }).then(async (response) => {
+        const current = response.data as Group
+
+        const changes = diff(
+          current.attributes,
+          values,
+        ) as Forms.Groups.UpdateValues
+        if (Object.keys(changes).length === 0) return current
+
+        const updated = await keygen.groups
+          .update({ id: groupId, values: changes })
+          .then((response) => response.data as Group)
+
+        return updated
+      }),
+
+    onSuccess: async (updated) => {
+      queryClient.setQueryData(
+        ["groups", groupId, { environment: code }],
+        updated,
+      )
+      await queryClient.invalidateQueries({
+        queryKey: ["groups", { environment: code }],
+      })
+    },
+  })
+}
+
+export function useRemoveGroup(groupId: string) {
+  const queryClient = useQueryClient()
+  const { code } = useEnvironment()
+
+  return useMutation({
+    mutationFn: () => keygen.groups.remove({ id: groupId }),
+
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({
+        queryKey: ["groups", { environment: code }],
+      })
+      queryClient.removeQueries({
+        queryKey: ["groups", groupId, { environment: code }],
+      })
+    },
+  })
+}

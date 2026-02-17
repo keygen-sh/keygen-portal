6597a5613ed9138f580c68e1b1fc1ba20fbab8b5
Cazden Gabrielse <cgabrielse@outlook.com>
2026-02-17 17:25:49 -0600

    add checkout license api endpoint

---
+++ src/keygen/licenses/check-out.ts
@@ -0,0 +1,37 @@
+import config from "@/keygen/config"
+import client from "@/keygen/client"
+
+import { LicenseFileResponse } from "@/types/licenses"
+
+config.validate()
+
+export interface CheckOutProps {
+  id: string
+  include?: string[]
+  ttl?: number
+  algorithm: string
+}
+
+export default async function checkOut({
+  id,
+  include,
+  ttl,
+  algorithm,
+}: CheckOutProps): Promise<LicenseFileResponse> {
+  const params = new URLSearchParams({ algorithm })
+
+  if (include) {
+    params.set("include", include.join(","))
+  }
+
+  if (ttl) {
+    params.set("ttl", String(ttl))
+  }
+
+  const result = (await client.request(
+    `/accounts/${config.id}/licenses/${id}/actions/check-out?${params}`,
+    { method: "POST" },
+  )) as LicenseFileResponse
+
+  return result
+}

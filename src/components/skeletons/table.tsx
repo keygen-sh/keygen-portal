50137d53e658eee2a4bea3b630ae06c06afb2c7a
cazden <cgabrielse@outlook.com>
2026-03-10 17:14:12 -0500

    remove pagination from table skeleton

--- src/components/skeletons/table.tsx
+++ src/components/skeletons/table.tsx
@@ -1,16 +1,26 @@
 import { Skeleton } from "@/components/ui/skeleton"
 
-export default function SkeletonTable(): React.ReactElement {
+import { cn } from "@/lib/utils"
+
+interface SkeletonTableProps {
+  className?: string
+}
+
+export default function SkeletonTable({
+  className,
+}: SkeletonTableProps): React.ReactNode {
   return (
-    <div className="flex w-full flex-col justify-center space-y-4 p-4 md:pr-12">
-      <Skeleton className="mb-4 h-6 w-full" />
+    <div
+      className={cn(
+        "flex w-full flex-col justify-center space-y-4 p-4 md:pr-12",
+        className,
+      )}
+    >
+      <Skeleton className="mb-4 h-6 w-full rounded-sm" />
       <Skeleton className="ml-4 h-8 w-3/4" />
       <Skeleton className="ml-4 h-8 w-1/2" />
       <Skeleton className="ml-4 h-8 w-1/4" />
       <Skeleton className="ml-4 h-8 w-1/3" />
-      <div className="flex w-full justify-end">
-        <Skeleton className="h-8 w-36" />
-      </div>
     </div>
   )
 }

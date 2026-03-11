6566938793962ba8274decc915f98e6db36b466e
cazden <cgabrielse@outlook.com>
2026-03-11 12:13:47 -0500

    update table skeleton styles

--- src/components/skeletons/table.tsx
+++ src/components/skeletons/table.tsx
@@ -10,17 +10,14 @@ export default function SkeletonTable({
   className,
 }: SkeletonTableProps): React.ReactNode {
   return (
-    <div
-      className={cn(
-        "flex w-full flex-col justify-center space-y-4 p-4 md:pr-12",
-        className,
-      )}
-    >
+    <div className={cn("w-full flex-1 px-4 py-2 md:pr-12", className)}>
       <Skeleton className="mb-4 h-6 w-full rounded-sm" />
-      <Skeleton className="ml-4 h-8 w-3/4" />
-      <Skeleton className="ml-4 h-8 w-1/2" />
-      <Skeleton className="ml-4 h-8 w-1/4" />
-      <Skeleton className="ml-4 h-8 w-1/3" />
+      <div className="flex w-full flex-col justify-center space-y-4 px-2">
+        <Skeleton className="ml-4 h-8 w-3/4" />
+        <Skeleton className="ml-4 h-8 w-1/2" />
+        <Skeleton className="ml-4 h-8 w-1/4" />
+        <Skeleton className="ml-4 h-8 w-1/3" />
+      </div>
     </div>
   )
 }

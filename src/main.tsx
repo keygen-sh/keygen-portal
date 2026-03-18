fe6430d2c3f122bc48d4cabebd4daa9527f7112b
Zeke Gabrielse <ezekg@yahoo.com>
2026-03-18 17:02:35 -0500

    update query client to not refetch as often

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,14 @@ import { routeTree } from "./routeTree.gen"
 
 import * as Page from "@/pages/index"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // don't refetch if data is < 5m old
+      refetchOnWindowFocus: false, // disable refetch on refocus
+    },
+  },
+})
 
 const router = createRouter({
   routeTree,

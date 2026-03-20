b0d0ae7f7288b9b0f2a179386f0d7738a3c37c60
cazden <cgabrielse@outlook.com>
2026-03-20 16:24:15 -0500

    add optional tag to card selector

--- src/components/forms/field/card-selector.tsx
+++ src/components/forms/field/card-selector.tsx
@@ -1,7 +1,9 @@
+import { Separator } from "@/components/ui/separator"
 import { cn } from "@/lib/utils"
 
 interface CardSelectorProps {
   title?: string
+  optional?: boolean
   multiselect?: boolean
   children: React.ReactNode
   className?: string
@@ -9,6 +11,7 @@ interface CardSelectorProps {
 
 export default function CardSelector({
   title,
+  optional = false,
   multiselect,
   children,
   className,
@@ -16,14 +19,20 @@ export default function CardSelector({
   return (
     <div className={cn("space-y-3 p-4", className)}>
       <div className="flex justify-between">
-        {title && (
-          <h2 className="mr-4 font-owners-wide text-sm font-medium tracking-wider text-content-normal uppercase">
-            {title}
-          </h2>
+        <div className="flex items-center gap-2">
+          {title && (
+            <h2 className="font-owners-wide text-sm font-medium tracking-wider text-content-normal uppercase">
+              {title}
+            </h2>
+          )}
+          <Separator orientation="vertical" />
+          <span className="font-owners-text text-xs font-normal tracking-wider text-content-subdued uppercase">
+            {multiselect ? "Select all that apply" : "Select one"}
+          </span>
+        </div>
+        {optional && (
+          <span className="text-xs text-content-subdued">Optional</span>
         )}
-        <span className="font-owners-text text-xs font-normal tracking-wider text-content-subdued uppercase">
-          {multiselect ? "Select all that apply" : "Select one"}
-        </span>
       </div>
       {children}
     </div>

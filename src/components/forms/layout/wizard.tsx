d8302ca033a8ba16511c6c73e982e00bec44b0b1
Zeke Gabrielse <ezekg@yahoo.com>
2026-03-20 13:59:36 -0500

    rename guard complete to close

--- src/components/forms/layout/wizard.tsx
+++ src/components/forms/layout/wizard.tsx
@@ -119,7 +119,7 @@ export default function FormsContentWizard<
         try {
           await onSubmit(data as T)
           if (autoClose) {
-            guard.complete()
+            guard.close()
           } else {
             form.reset()
           }

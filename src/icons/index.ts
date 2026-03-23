6582de5dbfea89a0304d079f924de19d83d37592
cazden <cgabrielse@outlook.com>
2026-03-23 14:52:13 -0500

    add engine icons

---
+++ src/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as PyPi } from "./pypi.svg?react"
+export { default as Tauri } from "./tauri.svg?react"
+export { default as RubyGems } from "./rubygems.svg?react"
+export { default as Npm } from "./npm.svg?react"

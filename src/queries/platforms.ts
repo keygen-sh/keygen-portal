1a20265cb1de4a5a8ec0e74e03be45013b8e7177
cazden <cgabrielse@outlook.com>
2026-03-26 12:24:20 -0500

    add platforms query hooks

---
+++ src/queries/platforms.ts
@@ -0,0 +1,51 @@
+import { useQuery, keepPreviousData } from "@tanstack/react-query"
+
+import { useEnvironment } from "@/hooks/use-environment"
+
+import { APIError } from "@/types/api"
+
+import * as keygen from "@/keygen"
+
+export function useGetPlatform(platformId: string) {
+  const { code } = useEnvironment()
+
+  return useQuery({
+    queryKey: ["platforms", platformId, { environment: code }],
+    queryFn: async () => {
+      const response = await keygen.platforms.get({ id: platformId })
+
+      if (!response.data) {
+        throw new Error("Platform not found")
+      }
+
+      return response.data
+    },
+    enabled: !!platformId,
+  })
+}
+
+export function useListPlatforms(params?: { page: number; pageSize: number }) {
+  const { code } = useEnvironment()
+
+  const query = useQuery({
+    queryKey: ["platforms", { environment: code, ...params }],
+    queryFn: async () => {
+      const response = await keygen.platforms.list(
+        params ? { pageNumber: params.page, pageSize: params.pageSize } : {},
+      )
+
+      if (response.errors) {
+        throw new APIError(response.errors[0])
+      }
+
+      return response
+    },
+    placeholderData: params ? keepPreviousData : undefined,
+  })
+
+  return {
+    ...query,
+    data: query.data?.data ?? [],
+    links: query.data?.links,
+  }
+}

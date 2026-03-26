590cf56b436fc8368b913e916942035c8cd000d9
cazden <cgabrielse@outlook.com>
2026-03-26 13:18:32 -0500

    add arches query hooks

---
+++ src/queries/arches.ts
@@ -0,0 +1,51 @@
+import { useQuery, keepPreviousData } from "@tanstack/react-query"
+
+import { useEnvironment } from "@/hooks/use-environment"
+
+import { APIError } from "@/types/api"
+
+import * as keygen from "@/keygen"
+
+export function useGetArch(archId: string) {
+  const { code } = useEnvironment()
+
+  return useQuery({
+    queryKey: ["arches", archId, { environment: code }],
+    queryFn: async () => {
+      const response = await keygen.arches.get({ id: archId })
+
+      if (!response.data) {
+        throw new Error("Arch not found")
+      }
+
+      return response.data
+    },
+    enabled: !!archId,
+  })
+}
+
+export function useListArches(params?: { page: number; pageSize: number }) {
+  const { code } = useEnvironment()
+
+  const query = useQuery({
+    queryKey: ["arches", { environment: code, ...params }],
+    queryFn: async () => {
+      const response = await keygen.arches.list(
+        params ? { pageNumber: params.page, pageSize: params.pageSize } : {},
+      )
+
+      if (response.errors) {
+        throw new APIError(response.errors[0])
+      }
+
+      return response
+    },
+    placeholderData: params ? keepPreviousData : undefined,
+  })
+
+  return {
+    ...query,
+    data: query.data?.data ?? [],
+    links: query.data?.links,
+  }
+}

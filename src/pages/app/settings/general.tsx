f3e220be3960b699d29c3a2e4a9cf05cc46b7bd5
cazden <cgabrielse@outlook.com>
2026-04-13 16:37:35 -0500

    add user profile page components

--- src/pages/app/settings/general.tsx
+++ src/pages/app/settings/general.tsx
@@ -4,13 +4,16 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 import { Button } from "@/components/ui/button"
 
 import { useGetAccount } from "@/queries/accounts"
+import { useGetCurrentUser } from "@/queries/users"
 
 import { useMobile } from "@/hooks/use-mobile"
 
+import { UserAttributeDescriptions } from "@/types/users"
 import { AccountAttributeDescriptions } from "@/types/accounts"
 
 import { copyToClipboard } from "@/lib/clipboard"
 
+import * as Users from "@/components/users"
 import * as Motion from "@/components/motion"
 import * as Account from "@/components/account"
 import * as Attribute from "@/components/attribute"
@@ -21,8 +24,10 @@ export default function General() {
   const isMobile = useMobile()
 
   const { data: account } = useGetAccount()
+  const { data: user } = useGetCurrentUser()
 
   const [editingAccount, setEditingAccount] = useState(false)
+  const [editingProfile, setEditingProfile] = useState(false)
 
   return (
     <section className="flex h-screen flex-col">
@@ -91,6 +96,77 @@ export default function General() {
                               type="raw"
                               value={account.attributes.slug}
                               tooltip={AccountAttributeDescriptions.slug}
+                            />
+                          }
+                        />
+                      </div>
+                    </div>
+                  )}
+                </Motion.Resize>
+              )}
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <h2 className="font-owners-wide text-lg text-content-loud">
+                Current User
+              </h2>
+              <p className="font-owners-text text-sm text-content-muted">
+                Manage current user settings and information.
+              </p>
+            </div>
+            <div className="overflow-hidden rounded bg-background-1">
+              {user && (
+                <Motion.Resize layoutKey={editingProfile ? "edit" : "view"}>
+                  {editingProfile ? (
+                    <Users.Form.Profile
+                      title="Editing profile"
+                      onClose={() => setEditingProfile(false)}
+                    />
+                  ) : (
+                    <div className="flex flex-col">
+                      <div className="flex items-center justify-end border-b border-accent p-2">
+                        <Button
+                          variant="outline"
+                          size="sm"
+                          onClick={() => setEditingProfile(true)}
+                          className="border-none bg-background-2"
+                        >
+                          Edit Profile
+                        </Button>
+                      </div>
+                      <div className="flex flex-col gap-4 p-4">
+                        <Attribute.Field
+                          label="Email"
+                          variant="none"
+                          value={
+                            <Attribute.Value
+                              type="raw"
+                              value={user.attributes.email}
+                              tooltip={UserAttributeDescriptions.email}
+                            />
+                          }
+                        />
+                        <Attribute.Field
+                          label="First name"
+                          variant="none"
+                          value={
+                            <Attribute.Value
+                              type="string"
+                              value={user.attributes.firstName}
+                              tooltip={UserAttributeDescriptions.firstName}
+                              emptyLabel="Not set"
+                            />
+                          }
+                        />
+                        <Attribute.Field
+                          label="Last name"
+                          variant="none"
+                          value={
+                            <Attribute.Value
+                              type="string"
+                              value={user.attributes.lastName}
+                              tooltip={UserAttributeDescriptions.lastName}
+                              emptyLabel="Not set"
                             />
                           }
                         />
--- src/pages/app/settings/index.ts
+++ src/pages/app/settings/index.ts
@@ -1,3 +1,4 @@
 export { default as General } from "./general"
+export { default as Security } from "./security"
 export { default as Permissions } from "./permissions"
 export { default as Developers } from "./developers"

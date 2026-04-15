c6593b3a8e5cf0ab047a00721e7251a1348d955b
cazden <cgabrielse@outlook.com>
2026-04-15 11:06:20 -0500

    add second factor to security settings

--- src/pages/app/settings/security.tsx
+++ src/pages/app/settings/security.tsx
@@ -1,7 +1,12 @@
 import { useState } from "react"
 
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
+
+import { SecondFactorMode } from "@/types/second-factors"
+
+import { useListSecondFactors } from "@/queries/second-factors"
 
 import * as Users from "@/components/users"
 import * as Motion from "@/components/motion"
@@ -9,6 +14,15 @@ import PageHeader from "@/components/page-header"
 
 export default function SecurityPage() {
   const [editingPassword, setEditingPassword] = useState(false)
+  const [secondFactorMode, setSecondFactorMode] = useState<SecondFactorMode>(
+    SecondFactorMode.View,
+  )
+
+  const { data: secondFactors = [], isLoading } = useListSecondFactors()
+
+  const enabledFactor = secondFactors.find((f) => f.attributes.enabled)
+  const orphanedFactor = secondFactors.find((f) => !f.attributes.enabled)
+  const isEnabled = !!enabledFactor
 
   return (
     <section className="flex h-screen flex-col">
@@ -42,6 +56,75 @@ export default function SecurityPage() {
                     >
                       Change Password
                     </Button>
+                  </div>
+                )}
+              </Motion.Resize>
+            </div>
+
+            <div className="flex flex-col space-y-2">
+              <h2 className="font-owners-wide text-lg text-content-loud">
+                Two-Factor
+              </h2>
+              <p className="font-owners-text text-sm text-content-muted">
+                Manage two-factor authentication.
+              </p>
+            </div>
+            <div className="overflow-hidden rounded bg-background-1">
+              <Motion.Resize layoutKey={secondFactorMode}>
+                {!isLoading && secondFactorMode === SecondFactorMode.Enable ? (
+                  <Users.Form.SecondFactor.Enable
+                    orphanedFactor={orphanedFactor}
+                    onClose={() => setSecondFactorMode(SecondFactorMode.View)}
+                  />
+                ) : !isLoading &&
+                  secondFactorMode === SecondFactorMode.Disable &&
+                  enabledFactor ? (
+                  <Users.Form.SecondFactor.Disable
+                    factor={enabledFactor}
+                    onClose={() => setSecondFactorMode(SecondFactorMode.View)}
+                  />
+                ) : (
+                  <div className="flex flex-col">
+                    <div className="flex items-center justify-end border-b border-accent p-2">
+                      {isEnabled ? (
+                        <Button
+                          variant="outline"
+                          size="sm"
+                          onClick={() =>
+                            setSecondFactorMode(SecondFactorMode.Disable)
+                          }
+                          className="border-none bg-background-2"
+                        >
+                          Deactivate
+                        </Button>
+                      ) : (
+                        <Button
+                          variant="outline"
+                          size="sm"
+                          onClick={() =>
+                            setSecondFactorMode(SecondFactorMode.Enable)
+                          }
+                          className="border-none bg-background-2"
+                        >
+                          Enable
+                        </Button>
+                      )}
+                    </div>
+                    <div className="flex flex-col gap-4 p-4">
+                      <p className="mr-auto text-sm text-content-muted">
+                        Your two-factor authentication is currently{" "}
+                        {isEnabled ? (
+                          <>
+                            <Badge variant="success" className="ml-0.5">
+                              Enabled
+                            </Badge>
+                            .
+                          </>
+                        ) : (
+                          <span>disabled.</span>
+                        )}
+                      </p>
+                    </div>
                   </div>
                 )}
               </Motion.Resize>

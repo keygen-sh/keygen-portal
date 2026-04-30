a317f855ff92e987741c523b708a6cbf42858021
cazden <cgabrielse@outlook.com>
2026-04-30 13:34:29 -0500

    refactor sso flow to login page

--- src/routes/$accountId/auth.sso.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router"
-import * as Page from "@/pages/index"
-
-export const Route = createFileRoute("/$accountId/auth/sso")({
-  component: () => <Page.Auth.SSO />,
-})

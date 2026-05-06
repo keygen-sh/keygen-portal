67ffe79311b24521971b98461215083ee7fc3065
cazden <cgabrielse@outlook.com>
2026-05-06 12:35:40 -0500

    refactor cloud definition to mirror api

--- src/hooks/use-cloud.ts
+++ src/hooks/use-cloud.ts
@@ -1,12 +1,5 @@
-import { useGetAccount } from "@/queries/accounts"
+import config from "@/keygen/config"
 
-export function useCloud(): { isCloud: boolean; isLoading: boolean } {
-  const { data: account, isLoading } = useGetAccount()
-
-  const isCloud = !!(
-    account?.relationships.plan?.data?.id ??
-    account?.relationships.billing?.data?.id
-  )
-
-  return { isCloud, isLoading }
+export function useCloud(): { isCloud: boolean } {
+  return { isCloud: config.isCloud }
 }
--- src/keygen/config.ts
+++ src/keygen/config.ts
@@ -1,7 +1,12 @@
+const CLOUD_HOSTS = ["api.keygen.sh", "api.keygen.dev"]
+
 const config = {
   host: import.meta.env.VITE_KEYGEN_HOST,
   mode: import.meta.env.VITE_KEYGEN_MODE,
-  isCloud: import.meta.env.VITE_KEYGEN_MODE === "multiplayer",
+  isCloud:
+    import.meta.env.VITE_KEYGEN_EDITION === "EE" &&
+    import.meta.env.VITE_KEYGEN_MODE === "multiplayer" &&
+    CLOUD_HOSTS.includes(import.meta.env.VITE_KEYGEN_HOST),
   version: import.meta.env.VITE_KEYGEN_VERSION,
   id: import.meta.env.VITE_KEYGEN_ACCOUNT_ID,
 

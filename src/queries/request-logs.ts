0e2bd951bd09e4970b65d434504792dd7a6ea6b6
cazden <cgabrielse@outlook.com>
2026-06-08 16:58:09 -0500

    add request logs query hooks

---
+++ src/queries/request-logs.ts
@@ -0,0 +1,49 @@
+import { useQuery } from "@tanstack/react-query"
+
+import { useEnvironment } from "@/hooks/use-environment"
+
+import { APIError } from "@/types/api"
+import { type RequestLogFilters } from "@/types/request-logs"
+
+import * as keygen from "@/keygen"
+
+export type { RequestLogFilters }
+
+export function useListRequestLogs(
+  params?: {
+    cursor?: string | null
+    pageSize?: number
+    filters?: RequestLogFilters
+  },
+  options?: { enabled?: boolean },
+) {
+  const { code } = useEnvironment()
+
+  const query = useQuery({
+    queryKey: ["request-logs", { environment: code, ...params }],
+    queryFn: async () => {
+      const response = await keygen.requestLogs.list(
+        params
+          ? {
+              pageCursor: params.cursor,
+              pageSize: params.pageSize,
+              filters: params.filters,
+            }
+          : {},
+      )
+
+      if (response.errors) {
+        throw new APIError(response.errors[0])
+      }
+
+      return response
+    },
+    enabled: options?.enabled,
+  })
+
+  return {
+    ...query,
+    data: query.data?.data ?? [],
+    links: query.data?.links,
+  }
+}

a874038f73d1ce588ed4c65800d7da0c09145b6c
Zeke Gabrielse <ezekg@yahoo.com>
2026-06-10 09:14:32 -0500

    update mix function sig

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,11 +1,9 @@
-export function mix({
-  baseColor,
-  mixedColor,
-  mixedColorAmount,
-}: {
-  baseColor: string
-  mixedColor: string
-  mixedColorAmount: number
-}) {
-  return `color-mix(in oklch, ${baseColor} ${100 - mixedColorAmount}%, ${mixedColor} ${mixedColorAmount}%)`
+type MixOptions = {
+  color: string
+  with: string
+  amount: number
+}
+
+export function mix({ color, with: color2, amount }: MixOptions) {
+  return `color-mix(in oklch, ${color} ${100 - amount}%, ${color2} ${amount}%)`
 }

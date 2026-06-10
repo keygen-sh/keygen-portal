80c6e59492f2a214bf96495cb88436b4e452b88e
cazden <cgabrielse@outlook.com>
2026-06-10 15:34:57 -0500

    add webhook endpoints table columns

---
+++ src/hooks/use-endpoint-table-columns.tsx
@@ -0,0 +1,52 @@
+import { useMemo } from "react"
+import { formatDate } from "date-fns"
+
+import { type Endpoint } from "@/types/endpoints"
+
+import { createTableColumnHelper } from "@/lib/tables"
+
+import ClipboardButton from "@/components/clipboard-button"
+
+const column = createTableColumnHelper<Endpoint>()
+
+export function useEndpointTableColumns() {
+  const columns = useMemo(
+    () => [
+      column.id({
+        header: "ID",
+        cell: (info) => (
+          <ClipboardButton value={info.getValue()} maxLength={8} />
+        ),
+      }),
+      column.attr("url", {
+        header: "URL",
+        cell: (info) => (
+          <span className="font-mono text-xs text-content-normal">
+            {info.getValue()}
+          </span>
+        ),
+      }),
+      column.attr("created", {
+        sortingFn: "datetime",
+        header: "Created",
+        cell: (info) => (
+          <span className="font-mono text-xs text-content-normal">
+            {formatDate(new Date(String(info.getValue())), "PP")}
+          </span>
+        ),
+      }),
+      column.attr("updated", {
+        sortingFn: "datetime",
+        header: "Updated",
+        cell: (info) => (
+          <span className="font-mono text-xs text-content-normal">
+            {formatDate(new Date(String(info.getValue())), "PP")}
+          </span>
+        ),
+      }),
+    ],
+    [],
+  )
+
+  return columns
+}

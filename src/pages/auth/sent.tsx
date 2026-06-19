6dbb58f57eb60896ed12d90bc2e25ef9cd2757d7
cazden <cgabrielse@outlook.com>
2026-06-19 10:04:41 -0500

    remove unused path prop from back button

--- src/pages/auth/sent.tsx
+++ src/pages/auth/sent.tsx
@@ -4,11 +4,7 @@ export default function Sent() {
   return (
     <section className="flex w-80 flex-col justify-center">
       <div className="flex flex-col space-y-4">
-        <BackButton
-          path="/auth/login"
-          label="Return to Login"
-          className="justify-start md:hidden"
-        />
+        <BackButton label="Return to Login" className="justify-start md:hidden" />
         <h1 className="bg-gradient-to-r from-brand-primary to-brand-secondary bg-clip-text font-owners-wide text-2xl font-medium text-transparent select-none">
           Check your inbox
         </h1>

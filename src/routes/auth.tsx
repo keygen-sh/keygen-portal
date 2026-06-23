861504d7b73c6b9c767ea546aa20c24c89114a04
cazden <cgabrielse@outlook.com>
2026-06-23 17:21:25 -0500

    add account form redirect for singleplayer

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,7 +1,18 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, redirect } from "@tanstack/react-router"
 
 import * as Layout from "@/layouts/index"
+import * as keygen from "@/keygen"
 
 export const Route = createFileRoute("/auth")({
+  beforeLoad: () => {
+    if (keygen.config.hasFixedAccount) {
+      redirect({
+        to: "/$accountId/auth/login",
+        params: { accountId: keygen.config.id },
+        replace: true,
+        throw: true,
+      })
+    }
+  },
   component: () => <Layout.Auth />,
 })

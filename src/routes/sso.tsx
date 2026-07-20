88aca914b8dee2dda49cd1adfa427eff0f43eda7
cazden <cgabrielse@outlook.com>
2026-07-20 17:10:24 -0500

    refactor sso error handling

--- src/routes/sso.tsx
+++ src/routes/sso.tsx
@@ -1,8 +1,5 @@
-import { createFileRoute } from "@tanstack/react-router"
-import * as Layout from "@/layouts/index"
+import { createFileRoute, Outlet } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/sso")({
-  // FIXME(cazden) Misleading to have `sso` render an error layout, but `sso.error`
-  //               is our only nested route at the moment, so leaving as-is for now.
-  component: () => <Layout.Error />,
+  component: () => <Outlet />,
 })

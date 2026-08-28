479f023dcc06d3c100dd42f380ae5309e223eed0
cazden <cgabrielse@outlook.com>
2026-08-28 10:45:54 -0500

    refactor onboarding env to shared

--- src/components/onboarding/form/environment.tsx
+++ src/components/onboarding/form/environment.tsx
@@ -1,13 +1,11 @@
 import { useCallback } from "react"
-import { useForm, useWatch } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 
-import { Globe, GlobeLock } from "lucide-react"
+import { Globe } from "lucide-react"
 
 import * as Schemas from "@/schemas"
 import { IsolationStrategy } from "@/types/environments"
-
-import { useEnvironment } from "@/hooks/use-environment"
 
 import { useCreateEnvironment } from "@/queries/environments"
 
@@ -34,34 +32,22 @@ export default function EnvironmentOnboardingForm({
     defaultValues: {
       name: "",
       code: "",
-      isolationStrategy: IsolationStrategy.Isolated,
+      isolationStrategy: IsolationStrategy.Shared,
     },
   })
 
   const createEnvironment = useCreateEnvironment()
-  const { select } = useEnvironment()
-
-  const selectedStrategy = useWatch({
-    control: form.control,
-    name: "isolationStrategy",
-  })
 
   const handleSubmit = useCallback(
     async (values: Schemas.Environments.CreateValues) => {
-      const environment = await createEnvironment.mutateAsync(values)
+      await createEnvironment.mutateAsync(values)
 
       toast({
         message: "Environment created",
         variant: "success",
       })
-
-      try {
-        await select(environment.id, environment.attributes.code)
-      } catch (error) {
-        console.error(error)
-      }
     },
-    [createEnvironment, select],
+    [createEnvironment],
   )
 
   return (
@@ -72,73 +58,27 @@ export default function EnvironmentOnboardingForm({
           isPending={createEnvironment.isPending}
           description={
             <BadgeGroup prefix="Creating a new" suffix="environment">
-              {selectedStrategy === IsolationStrategy.Isolated ? (
-                <BadgeGroupItem>
-                  <GlobeLock />
-                  Isolated
-                </BadgeGroupItem>
-              ) : (
-                <BadgeGroupItem>
-                  <Globe />
-                  Shared
-                </BadgeGroupItem>
-              )}
+              <BadgeGroupItem>
+                <Globe />
+                Shared
+              </BadgeGroupItem>
             </BadgeGroup>
           }
           errorMessage="Failed to create environment"
           submitLabel="Submit"
         >
           <Forms.Section.Step
-            crumb="Isolation strategy"
-            fields={["isolationStrategy"]}
-          >
-            <div className="flex flex-col gap-4 p-6">
-              <Notice className="mb-2 max-w-3xl">
-                <Notice.Title>
-                  You can use environments to separate your production data from
-                  your integration and testing data.
-                </Notice.Title>
-                <Notice.Description>
-                  This is useful for testing new features or integrations
-                  without affecting production. For this, you'd want to use an
-                  isolated environment, which keeps its resources separate from
-                  other environments and cannot affect outside resources.
-                </Notice.Description>
-              </Notice>
-
-              <Forms.Field.CardSelector
-                title="Isolation strategy"
-                className="m-0 p-0"
-              >
-                <Environments.Form.Fields
-                  schema="create"
-                  include={["isolationStrategy"]}
-                />
-              </Forms.Field.CardSelector>
-
-              <DocumentationLink
-                page="environments"
-                section="isolation-strategies"
-                message="For more information on isolation strategies and their effects, see"
-              >
-                Notes on isolation.
-              </DocumentationLink>
-            </div>
-          </Forms.Section.Step>
-
-          <Forms.Section.Step
             crumb="Environment attributes"
             fields={["name", "code"]}
           >
             <Notice className="m-4 mb-0 max-w-3xl">
               <Notice.Title>
-                Add a name and code for your environment.
+                You can use environments to separate your production data from
+                your integration and testing data.
               </Notice.Title>
               <Notice.Description>
-                This should be something that helps you identify the
-                environment, such as "Dev", "Staging" or "Sandbox". The code is
-                a unique identifier for the environment, and is typically used
-                in API requests to specify which environment to use.
+                This is useful for testing new features or integrations without
+                affecting production.
               </Notice.Description>
             </Notice>
 
@@ -152,14 +92,29 @@ export default function EnvironmentOnboardingForm({
             </Forms.Field.Title>
 
             <Forms.Section.Card title="Environment attributes">
-              <Environments.Form.Fields
-                schema="create"
-                include={["code"]}
-                fieldVariant="stacking"
-              />
+              <>
+                <Environments.Form.Fields
+                  schema="create"
+                  include={["code"]}
+                  fieldVariant="stacking"
+                />
+                <Notice className="mt-4 w-fit">
+                  <Notice.Description>
+                    The code is a unique identifier for the environment, and is
+                    typically used in API requests to specify which environment
+                    to use.
+                  </Notice.Description>
+                </Notice>
+              </>
             </Forms.Section.Card>
 
-            <DocumentationLink page="environments" />
+            <DocumentationLink
+              page="environments"
+              section="isolation-strategies"
+              message="For more information on isolation strategies and their effects, see"
+            >
+              Notes on isolation.
+            </DocumentationLink>
           </Forms.Section.Step>
         </Forms.Layout.Wizard>
       </Forms.Container.Dialog>
